df303b7c443c94abbca648fa7a4b76278ac09e69
AfreenShaik1786 <afreenshaik604@gmail.com>
2026-08-18 19:34:27 +0530

    added edited maximum consecutive 1s

--- 01_Arrays/01_Single_Traversal/25_Maximum_Consecutive_1s.cpp
+++ 01_Arrays/01_Single_Traversal/25_Maximum_Consecutive_1s.cpp
@@ -1,29 +1,25 @@
 #include<iostream>
 #include<vector>
 using namespace std;
-int MaxConsecutive1s(vector<int>&nums){
-  int n=nums.size();
-  int count=0;
-  int maxcount=0;
-  for(int i=0;i<n;i++){
-      if(nums[i]!=0){
+int maxconsecutive1s(vector<int>&nums){
+    int n=nums.size();
+    int count=0;
+    int maxcount=0;
+for(int i=0;i<n;i++){
+    if(nums[i]==1){
         count++;
-      }else {
-        if(count>maxcount){
-          maxcount=count;
-          
-        }
+    }else {
           count=0;
-      }
-      if(count>maxcount){
+    }
+    if(count>maxcount){
         maxcount=count;
-      }
-  }
-  
-  return maxcount;
+    }
+
+}
+return maxcount;
 }
 int main(){
-  vector<int>nums={1, 0, 1, 0, 1, 0, 1};
-  cout<<MaxConsecutive1s(nums);
-  return 0;
+    vector<int>nums={1,1,1,0,1,1,0,1};
+    cout<<maxconsecutive1s(nums);
+    return 0;
 }
