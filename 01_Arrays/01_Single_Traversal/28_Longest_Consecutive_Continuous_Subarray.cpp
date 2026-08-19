ede3665dc431a3ca9f6a90658fe9fb33c22aedd0
AfreenShaik1786 <afreenshaik604@gmail.com>
2026-08-19 20:40:11 +0530

    added longest increasing continuous subarray

---
+++ 01_Arrays/01_Single_Traversal/28_Longest_Consecutive_Continuous_Subarray.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int LongestIncreasingConsecutiveSubarray(vector<int>&nums){
+    int n=nums.size();
+    if(n==0){
+        return 0;
+    }
+    int count=1;
+    int maxcount=1;
+    for(int i=1;i<n;i++){
+        if(nums[i]>nums[i-1]){
+            count++;
+        }else{
+            count=1;
+        }
+        if(count>maxcount){
+            maxcount=count;
+        }
+    }
+    return maxcount;
+    
+}
+int main(){
+    vector<int>nums={};
+    cout<<LongestIncreasingConsecutiveSubarray(nums);
+    return 0;
+
+}
